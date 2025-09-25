85e561e4b352588c673deb498bfd4307b2e65dc5
Matthew Glenn Roxas <mroxas@eceprog2.ecn.purdue.edu>
2025-09-25 17:24:34 -0400

    Licensing metric fixed

--- src/metrics/registry.py
+++ src/metrics/registry.py
@@ -7,7 +7,7 @@ def get_all_metrics() -> List[MetricItem]:
         ("performance", performance_metric.metric),
         ("bus factor", bus_factor_metric.metric),
         ("code quality", code_quality_metric.metric),
-        ("license check ", license_check.metric),
+        ("license check", license_check.metric),
         ("metric_b", metric_b.metric),
         ("metric_c", metric_c.metric),
         ("metric_d", metric_d.metric),
