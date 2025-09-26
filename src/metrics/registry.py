7bcca81ab2ce270b05d321c1020014510276e3e1
Matthew Glenn Roxas <mroxas@eceprog3.ecn.purdue.edu>
2025-09-26 14:45:19 -0400

    Dataset / code availability temporarily disabled

--- src/metrics/registry.py
+++ src/metrics/registry.py
@@ -1,6 +1,6 @@
 from typing import List
 from src.models.types import MetricFn, MetricItem
-from . import license_check, metric_b, metric_c, metric_d, performance_metric, code_quality_metric, bus_factor_metric, dataset_quality, ramp_up_time
+from . import license_check, metric_b, metric_c, metric_d, performance_metric, code_quality_metric, bus_factor_metric, dataset_quality, ramp_up_time, available_dataset_code
 
 def get_all_metrics() -> List[MetricItem]:
     return [
@@ -10,6 +10,7 @@ def get_all_metrics() -> List[MetricItem]:
         ("license check", license_check.metric),
         ("dataset quality", dataset_quality.metric),
         ("ramp up time", ramp_up_time.metric),
+        ("available dataset/code", available_dataset_code.metric),
         ("metric_b", metric_b.metric),
         ("metric_c", metric_c.metric),
         ("metric_d", metric_d.metric),
