6b27e4b79c7bc041550e25b285593624377d7850
Matthew Glenn Roxas <mroxas@eceprog5.ecn.purdue.edu>
2025-09-27 19:17:41 -0400

    Fixed metric order, added placeholder for size

--- src/metrics/registry.py
+++ src/metrics/registry.py
@@ -1,14 +1,15 @@
 from typing import List
 from src.models.types import MetricFn, MetricItem
-from . import license_check, metric_b, metric_c, metric_d, performance_metric, code_quality_metric, bus_factor_metric, dataset_quality, ramp_up_time, available_dataset_code
+from . import size, license_check, metric_b, metric_c, metric_d, performance_metric, code_quality_metric, bus_factor_metric, dataset_quality, ramp_up_time, available_dataset_code
 
 def get_all_metrics() -> List[MetricItem]:
     return [
-        ("performance_claims", performance_metric.metric),
-        ("bus_factor", bus_factor_metric.metric),
-        ("code_quality", code_quality_metric.metric),
-        ("license", license_check.metric),
-        ("dataset_quality", dataset_quality.metric),
         ("ramp_up_time", ramp_up_time.metric),
-        ("dataset_and_code_score", available_dataset_code.metric)
+        ("bus_factor", bus_factor_metric.metric),
+        ("performance_claims", performance_metric.metric),
+        ("license", license_check.metric),
+        ("size_score", size.metric),
+        ("dataset_and_code_score", available_dataset_code.metric),
+        ("dataset_quality", dataset_quality.metric),
+        ("code_quality", code_quality_metric.metric),
     ]
